f9b2bcb0d3eea76bc858d0bfe5dc556180188519
Dularish <dularish1993@gmail.com>
2017-08-17 08:11:15 +0530

    Changes after P4 Module

--- server.js
+++ server.js
@@ -4,6 +4,65 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var article = {
+  'article-one': {
+    'title': 'Cats',
+    'content': `
+    <div>
+        <h3>About cats</h3>
+        <img src="https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg" class="animalsImages" alt="Just a cat picture">
+    </div>
+    <div>
+        <p>This is just an ariticle about cats. They may look really cute but at the end of the day they are animals just
+            like Lion, they have natural hunting instincts. They may sometimes bring live prey to the owner. They do
+            it to their kittens, to teach them how to hunt this can be seen as a form of treating the owner as a part
+            of their family.
+        </p>
+        <p>I love cats</p>`
+  },
+  'article-two': {
+    'title': 'Dogs',
+    'content': `        <div>
+    <h3>About dogs</h3>
+    <img src="https://static.pexels.com/photos/67660/pexels-photo-67660.jpeg" class="animalsImages" alt="Just a dog picture">
+               </div>
+       <p>This is just an article about dogs. The innate quality that comes to our mind 
+           whenever, we hear of dogs is loyalty. 'Loyal like a dog'. Dogs have been humans' best
+           companion in the history. When wolves started to get close to humans, they
+           thought they they could work together to each others' benefits. I cannot think of any
+           other animal which is more close to humans than a dog.
+       </p>
+       <p>I love dogs</p>`
+  }
+}
+function getTemplate(data) {
+  var title = data.title;
+  var content = data.content;
+  var template = `
+<html>
+
+<head>
+    <title>
+        ${title}
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/ui/style.css">
+   
+</head>
+<body>
+    <a href="/">Get Back to Home</a>
+<hr>
+    <div class="container">
+            ${content}
+</div>
+</div>
+</body>
+
+</html>
+`;
+
+  return template;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -17,6 +76,16 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+app.get('/ui/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(getTemplate(article[articleName]));
+  //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+//app.get('/ui/article-two', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+//});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
