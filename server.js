e6a2cf84350b7593f240a3c75a6113d4590dacc2
Dularish <dularish1993@gmail.com>
2017-08-31 08:30:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,11 +99,13 @@ var getArticleDataFromDB = function(article){
   //console.log(err, res);
   //pool.end();
   if (err){
+      alert("failure");
       //resp.status(500).send(err.toString());
       return err.toString();
   }
   else
   {
+      alert("success");
       //resp.send(JSON.stringify(res));
       return JSON.stringify(res);
   }
