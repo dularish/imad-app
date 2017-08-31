4a897d54dc3b1540e371a13e42c512fb9907c9a4
Dularish <dularish1993@gmail.com>
2017-08-31 08:46:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ app.get('/ui/fromDB/:articleName', function (req, res) {
   else
   {
       //window.alert("success");
-      res.send(JSON.stringify(resp));
+      res.send(JSON.stringify(resp.rows));
       //return JSON.stringify(res);
   }
 });
